ded3920eddf339821506cee58dafafb60b5c2708
Bob Muller <bobmuller.f@gmail.com>
2019-02-10 13:46:09 +0100

    added flat list and flat

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
+import flats from '../../data/flats';
+import FlatList from './flatList.jsx';
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
   }
 
   render() {
     return (
       <div>
-
-      <div className="map-container">
-
-      </div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
       </div>
     );
   }
