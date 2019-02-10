013a4687e60a9e6f73e9aac0e54774595e850951
Bob Muller <bobmuller.f@gmail.com>
2019-02-10 13:39:17 +0100

    added style and app.jsx

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      <div className="map-container">
+
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
